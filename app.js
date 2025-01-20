319657d459b6622393234aa9bb8de3393dce1882
Marcos Papa <marcos_papa@hotmail.com>
2025-01-20 11:33:01 -0300

    funções: agregar amigos e atualizar lista de amigos

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+let campoAmigo = document.getElementById('amigo');
+let listaDeAmigos = document.getElementById('listaAmigos');
+
+
+function adicionarAmigo(){
+    if (campoAmigo.value == ''){
+        alert('Por favor, insira um nome.')
+    }else {
+        amigos.push(campoAmigo.value);
+        atualizarListaDeAmigos();
+    }
+    campoAmigo.value = '';
+}
+
+function atualizarListaDeAmigos(){
+    listaDeAmigos.innerHTML = "";
+    for (let i =0; i <= amigos.length-1; i++){
+        let novoAmigo = document.createElement('li');
+        novoAmigo.textContent = amigos[i];
+        listaDeAmigos.appendChild(novoAmigo);
+    }
+}
